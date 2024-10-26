b77949802d3c226a82e1929f76305c8b6e7962d5
gcdamazio <damaziogabrielle@gmail.com>
2024-10-26 18:21:59 -0400

    Gabi

--- src/main/java/Operations.java
+++ src/main/java/Operations.java
@@ -7,14 +7,14 @@ public class Operations {
 public Double sum(input1){
     Double sumValue = Console.getDoubleInput("Enter the value you want to sum.");
 
-    return (input1+sumValue);
+    return input1+sumValue;
 }
 
 //Subtraction
 public Double subtract(input1){
     Double subtractValue = Console.getDoubleInput("Enter the value you want to subtract.");
 
-    return (input1-subtractValue);
+    return input1-subtractValue;
 
 }
 
@@ -22,7 +22,7 @@ public Double subtract(input1){
 public Double multiply(input1){
     Double multiplyValue = Console.getDoubleInput("Enter the value you want to multiply.");
 
-    return (input1*multiplyValue);
+    return input1*multiplyValue;
 
 }
 
@@ -30,6 +30,6 @@ public Double multiply(input1){
 public Double divide(input1){
     Double divideValue = Console.getDoubleInput("Enter the value you want to divide.");
 
-    return (input1/divideValue);
+    return input1/divideValue;
 
 }
--- src/main/java/com/zipcodewilmington/scientificcalculator/Core.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Core.java
@@ -23,6 +23,47 @@ public class Core {
         double pow2 = Math.pow(input1,input2);
         return pow2;
     }
+
+
+    //add method
+    public Double sum(input1){
+        Double sumValue = Console.getDoubleInput("Enter the value you want to sum.");
+
+        return input1+sumValue;
+    }
+
+    //Subtraction
+    public Double subtract(input1){
+        Double subtractValue = Console.getDoubleInput("Enter the value you want to subtract.");
+
+        return input1-subtractValue;
+
+    }
+
+    //Multiplication
+    public Double multiply(input1){
+        Double multiplyValue = Console.getDoubleInput("Enter the value you want to multiply.");
+
+        return input1*multiplyValue;
+
+    }
+
+    //Division
+    public Double divide(input1){
+        Double divideValue = Console.getDoubleInput("Enter the value you want to divide.");
+
+        return input1/divideValue;
+
+    }
+
+    
+
 }
+
+
+
+
+
+
 
 

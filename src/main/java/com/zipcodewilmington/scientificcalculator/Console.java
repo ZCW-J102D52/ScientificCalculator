744c507b1f8d75cb8a79459709a7ac4137c78ebb
Enda Figueroa <efigueroa@Zipcoders-MacBook-Pro-2.local>
2024-10-27 00:48:54 -0400

    Added program cases, inital display, clear first number, and quit function.

--- src/main/java/com/zipcodewilmington/scientificcalculator/Console.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Console.java
@@ -18,22 +18,64 @@ public class Console {
     public static String getStringInput(String prompt) {
         Scanner scanner = new Scanner(System.in);
         println(prompt);
-        String userInput = scanner.nextLine();
-        return userInput;
+        String operator = scanner.nextLine().toLowerCase();
+        return operator;
     }
 
-    public static Integer getIntegerInput(String prompt) {
-        return null;
+    public static Integer getFirstIntegerInput(String prompt) {
+        Scanner scanner = new Scanner(System.in);
+        println(prompt);
+        Integer num1 = scanner.nextInt();
+        return num1;
     }
 
-    public static Double getDoubleInput(String prompt) {
-        return null;
+    public static Integer getSecondIntegerInput(String prompt) {
+        Scanner scanner = new Scanner(System.in);
+        println(prompt);
+        Integer num2 = scanner.nextInt();
+        return num2;
+    }
+
+    public static Double getFirstDoubleInput(String prompt) {
+        Scanner scanner = new Scanner(System.in);
+        println(prompt);
+        Double num1 = scanner.nextDouble();
+        return num1;
+    }
+
+    public static Double getSecondDoubleInput(String prompt) {
+        Scanner scanner = new Scanner(System.in);
+        println(prompt);
+        Double num2 = scanner.nextDouble();
+        return num2;
     }
 
     public static void clearTheScreen() {
         System.out.print("\033\143");
     }
 
+    public static void getBunny() {
+        System.out.println(" /)_/)");
+        System.out.println("( o.o )");
+        System.out.println(" > ^ <");
+    }
+
+    public static String sendToMem(String prompt) {
+        Scanner scanner = new Scanner(System.in);
+        println(prompt);
+        String wantMem = scanner.nextLine().toLowerCase();
+        return wantMem;
+    }
+
+    public static String toQuit(String prompt) {
+        Scanner scanner = new Scanner(System.in);
+        println(prompt);
+        String toQuit = scanner.nextLine().toLowerCase();
+        println("toquit value: "+toQuit);
+        return toQuit;
+    }
+
+// I am trying something new. let's see if this vanishes.
 
 
 

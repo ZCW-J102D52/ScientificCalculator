758b80e88db1ccda21243fd4a8c13a5b23cf837c
Maisha Nawar <mnawar@Zipcoders-MacBook-Pro.local>
2024-10-27 15:42:32 -0400

    added switch

--- src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
@@ -1,76 +1,27 @@
 package com.zipcodewilmington.scientificcalculator;
-import java.nio.file.attribute.UserDefinedFileAttributeView;
-import java.util.Scanner;
+
 /**
  * Created by leon on 2/9/18.
  */
 public class MainApplication {
     public static void main(String[] args) {
-//        Console.println("Welcome to my calculator!");
-//        String s = Console.getStringInput("Enter a string");
-//        Integer i = Console.getIntegerInput("Enter an integer");
-//        Double d = Console.getDoubleInput("Enter a double.");
-//
-//
-//        Console.println("The user input %s as a string", s);
-//        Console.println("The user input %s as a integer", i);
-//        Console.println("The user input %s as a d", d);
-//        // thought process
-        //use switch to implement core equations
-        // in the cases you would use whatever problem you want so if the case = a getDivide or getMultiply you print out the division code
-        // if nothing is used it will defult to 0
+        Console.println("Welcome to my calculator!");
+        /*String s = Console.getStringInput("Enter a string");
+        Integer i = Console.getIntegerInput("Enter an integer");
+        Double d = Console.getDoubleInput("Enter a double.");
 
-              Scanner sc = new Scanner(System.in);
-         /*     System.out.println("Press "*" for multiplication");
-              System.out.println("Press + for addition");
-              System.out.println("Your sum is");*/
-        System.out.println(0);
-        int Calculator= sc.nextInt();
-       {     int a = 3;
-               int b =5;
-            int getMultiply = 0;
-            for (int i = 0; i < a; i++) {
-                getMultiply += b;
+        Console.println("The user input %s as a string", s);
+        Console.println("The user input %s as a integer", i);
+        Console.println("The user input %s as a d", d);*/
 
-            }
+        Double firstInt = Console.getDoubleInput("Enter command");
+        String operator = Console.getStringInput("Enter operator");
+        Double secondInt = Console.getDoubleInput("Enter next command");
 
-            switch (Calculator) {
-                case '*':
-                    System.out.println(getMultiply);
-                 break;
-                 case 'multiply '
+        System.out.println( firstInt + operator + secondInt + "=" + Console.Calculation(operator, firstInt, secondInt));
 
 
-            }
-        }
-        // Caculator = sc.nextInt();
+    }
 
 
-             /*switch(Caculator)
-              {
-                  case 1: System.out.println(a);
-                  break;
-                 /* case 2: System.out.println(2+2);
-                  break;
-                  default: System.out.println(0);*/
-
-
-//        int y = 0;
-//       switch(y) {
-//           case 0:
-//               System.out.println("=0");
-//           case //get multiply:
-//               System.out.println(//sum);
-//                                  case (2*2)://getDivide
-//                                  System.out.println( );//sum
-//                                                     default:
-//                                                         System.print.println(return y);
-// breaks and well if I type additon
-       }
-
-        }
-
-
-
-
-
+}

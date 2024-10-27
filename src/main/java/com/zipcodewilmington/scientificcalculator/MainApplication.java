37c3ee4595fc407661c771cf8e7bc26bf7a4730e
rjohnson0851 <ron21johnson@comcast.net>
2024-10-27 14:02:12 -0400

    Made some changes

--- src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
@@ -8,10 +8,37 @@ public class MainApplication {
         Console.println("Welcome to my calculator!");
         String s = Console.getStringInput("Enter a string");
         Integer i = Console.getIntegerInput("Enter an integer");
+        Integer i2 = Console.getIntegerInput("Enter an integer");
         Double d = Console.getDoubleInput("Enter a double.");
 
         Console.println("The user input %s as a string", s);
         Console.println("The user input %s as a integer", i);
         Console.println("The user input %s as a d", d);
+
+        int answer = i / i2;
+
+
+        try {
+
+        } catch (ArithmeticException e) {
+            System.out.println("You are not allowed to divide by zero.");
+        } catch (Exception e) {
+            System.out.println("Error: Please try again");
+        } finally {
+            System.out.println("Please continue");
+        }
+
     }
-}
+    double memoryValue = 0.0;;
+    public void setMPlus(double value) {
+        memoryValue += value;
+    }
+    public double getMRC() {
+        return memoryValue;
+    }
+    public void getMC() {
+        memoryValue = 0.0;
+    }
+    }
+
+

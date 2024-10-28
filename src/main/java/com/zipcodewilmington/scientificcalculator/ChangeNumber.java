956b4a1cb6c10c8b685e61174bb3f79fbe62978e
Maisha Nawar <mnawar@Zipcoders-MacBook-Pro.local>
2024-10-28 08:17:36 -0400

    Formatted and added more cases

--- src/main/java/com/zipcodewilmington/scientificcalculator/ChangeNumber.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/ChangeNumber.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class ChangeNumber {
     public static void getChange() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("C - Change number"); //user has to press C in order to change number
+        System.out.println("Change number"); //user has to press C in order to change number
         int input1 = scanner.nextInt(); // allows the user to enter a new number
     }
 }
--- src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
@@ -1,11 +1,29 @@
 package com.zipcodewilmington.scientificcalculator;
+import com.zipcodewilmington.scientificcalculator.Scientific;
+
+import java.lang.Math;
 
 /**
  * Created by leon on 2/9/18.
  */
 public class MainApplication {
     public static void main(String[] args) {
-        Console.println("Welcome to my calculator!");
+
+        // codes for colors
+        String reset = "\u001B[0m";
+        String yellow = "\u001B[33m";
+        String cyan = "\u001B[36m";
+        String purple = "\u001B[35m";
+
+        // welcome message
+        System.out.println(cyan + "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" + reset);
+        System.out.println(cyan + "|" + reset + yellow + "   Welcome to Xylophanes Calculator!    "  + reset + cyan +
+                "|" + reset);
+        System.out.println(cyan + "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" + reset);
+        System.out.println();
+
+
+        //Console.println("Welcome to my calculator!");
         /*String s = Console.getStringInput("Enter a string");
         Integer i = Console.getIntegerInput("Enter an integer");
         Double d = Console.getDoubleInput("Enter a double.");
@@ -14,12 +32,18 @@ public class MainApplication {
         Console.println("The user input %s as a integer", i);
         Console.println("The user input %s as a d", d);*/
 
-        Double firstInt = Console.getDoubleInput("Enter command");
-        String operator = Console.getStringInput("Enter operator");
-        Double secondInt = Console.getDoubleInput("Enter next command");
+        int firstInt = Console.getIntegerInput("Enter a number below:");
+        String operator = Console.getStringInput("Enter operator:");
+        int secondInt = Console.getIntegerInput("Enter next number:");
         //Double ChangeNumber = Console.getChange();
 
-        System.out.println( firstInt + operator + secondInt +  "=" + Console.Calculation(operator, firstInt, secondInt));
+        System.out.println( "\nThe result of your calculation is: " + Console.Calculation(operator, firstInt, secondInt )+ "\n");
+
+        System.out.println(yellow + "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" + reset);
+        System.out.println(yellow + "|" + reset + purple + "   Numbers crunched, answers found. Thanks for using our calculator!    "  + reset + yellow +
+                "|" + reset);
+        System.out.println(yellow + "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" + yellow);
+        System.out.println();
 
 
     }

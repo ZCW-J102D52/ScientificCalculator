c801e24196e73d958c36282c490cc4b11ab32d5e
Alyssa Resh <aresh@Zipcoders-MacBook-Pro.local>
2024-10-30 17:02:24 -0400

    added tests for scientific functions, fixed my trig functions in Opera

--- src/main/java/com/zipcodewilmington/scientificcalculator/Opera.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Opera.java
@@ -62,31 +62,64 @@ public class Opera{
     //Factorial
     public static long factorial(long n1) {
         long result = 1L;
-        for (long i = 1; i <= n1; i++) {result *= i;
-            return result;
+        for (long i = 1; i <= n1; i++) {
+            result *= i;
+        }
+        return result;
+    }
+        //Log Value
+        public static double logFunction ( double n1){
+            double logvalue = Math.log(n1);
+            return logvalue;
+        }
+        //Log Function
+        public static double logFunction10 ( double n2){
+            double log10result = Math.log10(n2);
+            return log10result;
+        }
+        //Anti Log
+        public static double antiLog ( double n1){
+            double y = Math.log(n1); // Calculate the natural log of x
+            double inverselog = Math.exp(y);
+            //        double y = Math.log(n1) / Math.log(base); // Calculate log base 'base' of x
+            //        double inverselog = Math.pow(base, y);
+            return inverselog;
         }
 
-    //Log Value
-    public static double logFunction(double n1){
-        double logvalue = Math.log(n1);
-        return logvalue;}
-    //Log Function
-    public static double logFunction10(double n2) {
-        double log10result = Math.log10(n2);
-        return log10result;}
-    //Anti Log
-    public static double antiLog(double n1){
-        double y = Math.log(n1); // Calculate the natural log of x
-        double inverselog = Math.exp(y);
-        //        double y = Math.log(n1) / Math.log(base); // Calculate log base 'base' of x
-        //        double inverselog = Math.pow(base, y);
-        return inverselog; }
-    //
-    //////////////////////////////////////////////////////////////////////////////////////////////
+        //Sine
+        public static double sin(int deg){
+            double sineValue = Math.sin(Math.toRadians(deg));
+            return sineValue;
+        }
+        //Cosine
+        public static double cos(int deg){
+            double cosValue = Math.cos(Math.toRadians(deg));
+            return cosValue;
+        }
+        //Tangent
+        public static double tan(int deg){
+            double tanValue = Math.tan(Math.toRadians(deg));
+            return tanValue;
+        }
+
+    public static int invSin(double radians){
+        int invSin = (int)Math.toDegrees(Math.asin(radians));
+        return invSin;
+    }
+
+    public static int invCos(double radians){
+        int invCos = (int)Math.toDegrees(Math.acos(radians));
+        return invCos;
+    }
+
+    public static int invTan(double radians){
+        int invTan = (int)Math.toDegrees(Math.atan(radians));
+        return invTan;
+    }
+
+        //
+        //////////////////////////////////////////////////////////////////////////////////////////////
 
 
+    }
 
-
-
-
-}
--- src/test/java/com/zipcodewilmington/scientific_calculator/TestMainApplication.java
+++ src/test/java/com/zipcodewilmington/scientific_calculator/TestMainApplication.java
@@ -2,6 +2,7 @@ package com.zipcodewilmington.scientific_calculator;
 
 import com.zipcodewilmington.scientificcalculator.InverseFunction;
 import com.zipcodewilmington.scientificcalculator.MainApplication;
+import com.zipcodewilmington.scientificcalculator.Opera;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -12,36 +13,34 @@ import static org.junit.Assert.assertEquals;
  */
 public class TestMainApplication {
 
-
+    //Testing Scientific functions
     @Test
     public void testSquareRoot() {
-
-        MainApplication mainApplication = new MainApplication();
-        double result = mainApplication.squareRoot(4);
+        Opera operaSqRt = new Opera();
+        double result = operaSqRt.squareRoot(4);
         assertEquals(2, result, 0.001);
-
     }
 
     @Test
     public void testsquare() {
-        MainApplication mainApplication = new MainApplication();
-        double sq = mainApplication.square(2);
+        Opera operaSq = new Opera();
+        double sq = operaSq.square(2);
         assertEquals(4, sq, 0.001);
 
     }
 
     @Test
     public void exponentiation() {
-        MainApplication mainApplication = new MainApplication();
-        double exp = mainApplication.exponentiation(2);
+        Opera operaExp = new Opera();
+        double exp = operaExp.exponentiation(2);
         assertEquals(7.38905609893065, exp, 0.001);
 
     }
 
     @Test
     public void factorial() {
-        MainApplication mainApplication = new MainApplication();
-        double fact = mainApplication.factorial(3);
+        Opera operaFact = new Opera();
+        double fact = operaFact.factorial(3);
         assertEquals(6, fact, 0.001);
 
     }
@@ -64,23 +63,68 @@ public class TestMainApplication {
 
     @Test
     public void Logarithmicfunc() {
-        MainApplication mainApplication = new MainApplication();
-        double logresult = mainApplication.logFunction(10);
+        Opera operaLog = new Opera();
+        double logresult = operaLog.logFunction(10);
         assertEquals(2.302585, logresult, 0.001);
 
     }
+
     @Test
     public void Logarithmicfunc10() {
-        MainApplication mainApplication = new MainApplication();
-        double log10result = mainApplication.logFunction10(10);
+        Opera operaLog10 = new Opera();
+        double log10result = operaLog10.logFunction10(10);
         assertEquals(1, log10result, 0.001);
     }
 
     @Test
     public void inverseLogarithmicfunc() {
-        MainApplication mainApplication = new MainApplication();
-        double inverselogresult = mainApplication.antiLog(10);
+        Opera operaInvLog = new Opera();
+        double inverselogresult = operaInvLog.antiLog(10);
         assertEquals(10, inverselogresult, 0.001);
     }
 
+    @Test
+    public void sinTest() {
+        Opera operaSin = new Opera();
+        double sinResult = operaSin.sin(90);
+        assertEquals(1, sinResult);
+    }
+
+    @Test
+    public void cosTest() {
+        Opera operaCos = new Opera();
+        double cosResult = operaCos.cos(90);
+        assertEquals(0, cosResult);
+    }
+
+    @Test
+    public void tanTest() {
+        Opera operaTan = new Opera();
+        double tanResult = operaTan.tan(45);
+        assertEquals(1, tanResult);
+        //might need this as parameters for above (Opera.sin(45)/Opera.cos(45)
+    }
+
+    @Test
+    public void invSinTest() {
+        Opera operaInvSin = new Opera();
+        int invSinResult = operaInvSin.invSin(1);
+        assertEquals(90, invSinResult);
+    }
+
+    @Test
+    public void invCosTest() {
+        Opera operaInvCos = new Opera();
+        int invCosResult = operaInvCos.invCos(1);
+        assertEquals(0, invCosResult);
+    }
+
+    @Test
+    public void invTanTest() {
+        Opera operaInvTan = new Opera();
+        int invTanResult = operaInvTan.invTan(1);
+        assertEquals(45, invTanResult);
+    }
+
+}
 

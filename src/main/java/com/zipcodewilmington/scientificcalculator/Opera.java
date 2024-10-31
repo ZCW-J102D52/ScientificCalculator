773e3371a5c8f69a6459441b0f23e8fc908af7fd
Sharmin Jahan <sjahan@Zipcoders-MacBook-Pro.local>
2024-10-31 13:17:06 -0400

    done

--- src/main/java/com/zipcodewilmington/scientificcalculator/Opera.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Opera.java
@@ -11,6 +11,9 @@ public class Opera{
 
 
     //////////////////////////////   METHODS - SHARMIN  ///////////////////////////////////////
+    //Addition Method
+    public static double addition(double a, double b) {
+        return a + b;}
     //Subtraction Method
     public static double subtract(double a, double b) {
         return a - b; }
@@ -29,58 +32,58 @@ public class Opera{
     public static double radians(double a, double b) {
         double radians = 0;
         double degrees;
-        return radians;}
+        return radians;}}
     ///////////////////////////////   METHODS DEEPTI   ///////////////////////////////////////////
     // Methods to accept double input value
-    public static double acceptDoubleNum() {
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Enter any number: ");
-        double x = scan.nextInt();
-        scan.close();
-        return x;
-    }
-    // Methods to accept int input value
-    public static int acceptIntNum() {
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Enter any number: ");
-        int x = scan.nextInt();
-        scan.close();
-        return x;}
+//    public static double acceptDoubleNum() {
+//        Scanner scan = new Scanner(System.in);
+//        System.out.print("Enter any number: ");
+//        double x = scan.nextInt();
+//        scan.close();
+//        return x;
+//    }
+//    // Methods to accept int input value
+//    public static int acceptIntNum() {
+//        Scanner scan = new Scanner(System.in);
+//        System.out.print("Enter any number: ");
+//        int x = scan.nextInt();
+//        scan.close();
+//        return x;}
+//
+//    // Square
+//    public static double square(double x2) {
+//        double square = Math.pow(x2, 2);
+//        return square;}
+//    //Square Root
+//    public static double squareRoot(double x1) {
+//        double result = Math.sqrt(x1);
+//        return result;}
+//    //Exponential
+//    public static double exponentiation(double x3) {
+//        double exponent = Math.exp(x3);
+//        return exponent;}
+//    //Factorial
+//    public static long factorial(long n1) {
+//        long result = 1L;
+//        for (long i = 1; i <= n1; i++) {result *= i;
+//            return result;
+//        }
 
-    // Square
-    public static double square(double x2) {
-        double square = Math.pow(x2, 2);
-        return square;}
-    //Square Root
-    public static double squareRoot(double x1) {
-        double result = Math.sqrt(x1);
-        return result;}
-    //Exponential
-    public static double exponentiation(double x3) {
-        double exponent = Math.exp(x3);
-        return exponent;}
-    //Factorial
-    public static long factorial(long n1) {
-        long result = 1L;
-        for (long i = 1; i <= n1; i++) {result *= i;
-            return result;
-        }
-
-    //Log Value
-    public static double logFunction(double n1){
-        double logvalue = Math.log(n1);
-        return logvalue;}
-    //Log Function
-    public static double logFunction10(double n2) {
-        double log10result = Math.log10(n2);
-        return log10result;}
-    //Anti Log
-    public static double antiLog(double n1){
-        double y = Math.log(n1); // Calculate the natural log of x
-        double inverselog = Math.exp(y);
-        //        double y = Math.log(n1) / Math.log(base); // Calculate log base 'base' of x
-        //        double inverselog = Math.pow(base, y);
-        return inverselog; }
+//    //Log Value
+//    public static double logFunction(double n1){
+//        double logvalue = Math.log(n1);
+//        return logvalue;}
+//    //Log Function
+//    public static double logFunction10(double n2) {
+//        double log10result = Math.log10(n2);
+//        return log10result;}
+//    //Anti Log
+//    public static double antiLog(double n1){
+//        double y = Math.log(n1); // Calculate the natural log of x
+//        double inverselog = Math.exp(y);
+//        //        double y = Math.log(n1) / Math.log(base); // Calculate log base 'base' of x
+//        //        double inverselog = Math.pow(base, y);
+//        return inverselog; }
     //
     //////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -89,4 +92,4 @@ public class Opera{
 
 
 
-}
+//}

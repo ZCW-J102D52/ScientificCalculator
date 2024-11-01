eaea6d490d710d9483817ea52b3cc09b5bf98196
Ulas Yakut <uyakut@Zipcoders-MacBook-Pro-5.local>
2024-11-01 14:10:39 -0400

    commit

--- src/main/java/com/zipcodewilmington/scientificcalculator/Console.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Console.java
@@ -2,9 +2,6 @@ package com.zipcodewilmington.scientificcalculator;
 
 import java.util.Scanner;
 
-/**
- * Created by leon on 2/9/18.
- */
 public class Console {
 
     public static void print(String output, Object... args) {
@@ -18,98 +15,85 @@ public class Console {
     public static String getStringInput(String prompt) {
         Scanner scanner = new Scanner(System.in);
         println(prompt);
-        String userInput = scanner.nextLine();
-        return userInput;
+        return scanner.nextLine();
     }
 
     public static Integer getIntegerInput(String prompt) {
         Scanner scanner = new Scanner(System.in);
         println(prompt);
-        Integer userInput = scanner.nextInt();
-        return userInput;
+        return scanner.nextInt();
     }
 
     public static Double getDoubleInput(String prompt) {
         Scanner scanner = new Scanner(System.in);
         println(prompt);
-        Double userInput = scanner.nextDouble();
-        return userInput;
-
+        return scanner.nextDouble();
     }
 
-    public static Double getChange() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Change number"); //user has to press C in order to change number
-        Double input1 = scanner.nextDouble(); // allows the user to enter a new number
-        return input1;
-    }
-
-    public static Double Calculation (String operator, double x, double y) {
+    public static Double Calculation(String operator, double x, double y) {
         Core core = new Core();
-        Scientific scientific = new Scientific();
-
 
         switch (operator) {
-
-            case "-":
-            case "subtract":
-            case "minus":
-                return core.subtract(x, y);
-            case "+":
-            case "add":
+            case "1":
                 return core.sum(x, y);
-            case "*":
-            case "multiply":
+            case "2":
+                return core.subtract(x, y);
+            case "5":
                 return core.multiply(x, y);
-            case "divide":
-            case "/":
-                return core.divide(x, y);
-            case "exp":
-            case "^":
+            //case "6":
+            //return core.divide(x, y);
+            case "7":
                 return core.exp(x, y);
-            case "square":
-            case "sqr":
-                return core.square(x);
-            case "squareroot":
-            case "sqrt":
-                return core.squareRoot(x);
-            case "C":
-                return core.ChangeNumber(x, y);
-            case "sin":
-            case "sine":
-                return scientific.sine(x);
-            case "cosine":
-            case "cos":
-                return scientific.cosine(x);
-            case "tan":
-                return scientific.tangent(x);
-            case "inverse sin":
-            case "inverse sine":
-                return scientific.inverseSine(x);
-            case "inverse cosine":
-            case "inverse cos":
-                return scientific.inverseCosine(x);
-            case "inverse tan":
-            case "inverse tangent":
-                return scientific.inverseTangent(x);
-            case "log":
-                return scientific.logValue(x, y);
-            case "inverselog":
-                return scientific.inverseLog(x);
-            case "naturallog":
-                return scientific.naturalLog(x);
-            case "inversenaturallog":
-                return scientific.inverseNatLog(x);
-            case "factorial":
-            case "!":
-                return scientific.factorial(x);
-            case "%":
-            case "percent":
-            case "percentage":
-                return core.percentage(x , y);
+            case "8":
+                return core.percentage(x, y);
+            case "9":
+                return core.logValue(x, y);
             default:
-                System.out.println("Can not recognize operator. Try again!");
+                println("---Can not recognize operator. Try again!---");
+                return null;
+        }
+    }public static Double Calculation2(String operator, double x) {
+        Core core = new Core();
+
+        switch (operator) {
+            case "3":
+                return core.square(x);
+            case "4":
+                return core.squareRoot(x);
+            case "10":
+                return core.invertSign(x);
+            case "11":
+                return core.sine(x);
+            case "12":
+                return core.cosine(x);
+            case "13":
+                return core.tangent(x);
+            case "14":
+                return core.inverseLog(x);
+            case "15":
+                return core.naturalLog(x);
+            case "16":
+                return core.inverseNatLog(x);
+            case "17": // Celsius to Fahrenheit
+                return core.celsiusToFahrenheit(x);
+            case "18": // Fahrenheit to Celsius
+                return core.fahrenheitToCelsius(x);
+            default:
+                println("---Can not recognize operator. Try again!---");
+                return null;
+        }
+    }
+
+    public static String Calculation4(String operator, double x, double y) {
+        Core core = new Core();
+
+        switch (operator) {
+            case "6":
+                return core.divide(x, y);
+            default:
+                println("---Can not recognize operator. Try again!---");
                 return null;
         }
     }
 }
+
